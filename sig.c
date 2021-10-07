d9803f134483f96d220e184a16a9ff10cb4516e1
Suyash Vardhan Mathur <suyash.mathur@research.iiit.ac.in>
2021-10-08 01:13:25 +0530

    Add error handling in signal.

--- sig.c
+++ sig.c
@@ -3,6 +3,16 @@
 
 void sig(int job_no, int signal)
 {
+    if (job_no <= 0 || job_no > proc_no)
+    {
+        printf("Error: Invalid job no.\n");
+        return;
+    }
+    if ((signal < 1) || (signal > 64) || (signal == 16) || (signal == 32) || (signal == 33))
+    {
+        printf("Error: Invalid signal.\n");
+        return;
+    }
     int ret = kill(bg_jobs[job_no - 1].pid, signal);
     if (ret == -1)
     {
