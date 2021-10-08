c4f712295ad9c39cd67b370aac715d22d79a47f9
Suyash Vardhan Mathur <suyash.mathur@research.iiit.ac.in>
2021-10-08 08:56:45 +0530

    delete redundant files.

--- execute_command.h
+++
@@ -1,6 +0,0 @@
-#ifndef __EXECUTE_COMM_H
-#define __EXECUTE_COMM_H
-
-void execute_command(char *command, char **args, int i, char *history_file, char *home_dir, char *pwd, char *previous_directory);
-
-#endif
--- replay.h
+++
@@ -1,6 +0,0 @@
-#ifndef __REPLAY_H
-#define __REPLAY_H
-
-void replay(char **args, int period, int interval, char *history_file, int i);
-
-#endif
